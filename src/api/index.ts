cf9da078acfbbe991a9d7796d502bab3d1c9367c
IzzySato <izzy.sato@gmail.com>
2024-06-28 10:58:51 -0700

    updated space

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ const api: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/',
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
@@ -12,15 +12,15 @@ api.interceptors.request.use(config => {
   return config;
 });
 
-api.interceptors.response.use(response => response,
-  error => {
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
     if (error.response.status === 403) {
       localStorage.removeItem('token');
       window.location.href = '/login';
       return Promise.reject(error);
     }
   }
-)
+);
 
-
-export default api
+export default api;
