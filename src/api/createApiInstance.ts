79fdc54e4db19916a522dd83ac2d8a8d5d5e2fd1
IzzySato <izzy.sato@gmail.com>
2024-08-22 12:26:41 -0700

    fixed env

--- src/api/createApiInstance.ts
+++ src/api/createApiInstance.ts
@@ -3,6 +3,7 @@ import { createApiInstance } from '.';
 export const api = createApiInstance(
   process.env.REACT_APP_API_URL || 'http://localhost:8080/'
 );
+
 export const aiApi = createApiInstance(
-  process.env.AI_API_URL || 'http://127.0.0.1:5000/'
+  process.env.REACT_APP_AI_API_URL || 'http://127.0.0.1:5000/'
 );
